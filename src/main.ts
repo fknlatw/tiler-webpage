a878bde5095fdbab8d1f21350123b21873b66fbd
fknlatw <argat2480@gmail.com>
2026-09-09 21:38:23 +0200

    -add/header+main

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
 import "./style.css";
 
-document.getElementById("app")
+document.getElementById("app").innerHTML = `
+  <header class="border-gray-800 border-b bg-gray-900 text-gray-200 ">
+    <div class="flex px-6 py-4 mx-auto max-w-5xl">
+      flex
+    </div>
+  </header>
+  <main class="p-2">
+    <div class="p-4 rounded-xl bg-gray-900 border-gray-800 mx-auto border max-w-5xl">loremipsum</div>
+  </main>
+`
