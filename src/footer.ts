1a4a1300f080d4da4d2d54492d380b0460032b76
fknlatw <argat2480@gmail.com>
2026-09-12 22:12:02 +0200

    -add/footer

---
+++ src/footer.ts
@@ -0,0 +1,10 @@
+import "./style.css";
+
+document.getElementById("app").innerHTML += `
+  <footer class="p-8 text-slate-900 border-t border-slate-200 bg-white">
+    <div class="flex mx-auto gap-1 justify-between max-w-5xl">
+      <h1>FLTW</h1> 
+      <p>2026</p>
+    </div>
+  </footer>
+`;
